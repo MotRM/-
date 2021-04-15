afc509954f8c1ff0209c26fb5161aebe480c72ae
MotRM <motr.89@mail.ru>
2021-04-15 15:08:07 +1000

    Добавлены модули:  gamecity.py - сама игра (с двумя функциями);  infocity.py - будущий модуль для поиска информациио городе, пока пустой и используется для теста кода из gamecity;  CITIES.md - добавлен текстовы файл с городами.  В README.md добавлен описание бота. Добавил defaultdict, теперь каждому ключу несколько значений городов Отлажена функция игры в города, теперь она удаляет указанный город Добавлена функция определяющего первого игрока Исправлена функция определяющая первого игрока

--- gamecity.py
+++ gamecity.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 cities = defaultdict(list)
 city_key_bot = ''
 city_key_client = ''
+first_city = ''
+first_city_key = ''
 
 def dict_cities():
     """
@@ -27,7 +29,11 @@ def first_start_game(client_message_startgame):
         if random.randint(0, 5) > 2:
             return f'Вы называете город первым!'
         else:
-            return f'Я хожу первым!'
+            random_values_city = random.choice(list(cities.values()))
+            first_city = random_values_city[random.randint(0, len(random_values_city))]
+            first_city_key = first_city[0].lower()
+            cities[first_city_key].remove(first_city)
+            return f'Я хожу первым!\nГород:{first_city}'
     else:
         return f'Ну и сиди скучай!'
 
--- infocity.py
+++ infocity.py
@@ -1,9 +1,12 @@
 # тут пока тестирую код из модуля gamecity
 from collections import defaultdict
+import random
 
 cities = defaultdict(list)
 city_key_bot = ''
 city_key_client = ''
+first_city = ''
+first_city_key = ''
 city = 'Анапа'
 
 with open('CITIES.md', 'r', encoding='Windows-1251') as file:
@@ -11,6 +14,23 @@ with open('CITIES.md', 'r', encoding='Windows-1251') as file:
         cities[line[0].lower()].append(line[:-1])
 print(cities)
 
+def first_start_game(client_message_startgame):
+
+    """ Данная функция запускается при
+    вводе команды и определяет кто называет
+     город первым. """
+
+    if client_message_startgame == '$Игра':
+        if random.randint(0, 5) > 2:
+            return f'Вы называете город первым!'
+        else:
+            random_values_city = random.choice(list(cities.values()))
+            first_city = random_values_city[random.randint(0, len(random_values_city))]
+            first_city_key = first_city[0].lower()
+            cities[first_city_key].remove(first_city)
+            return f'Я хожу первым!\nГород:{first_city}'
+    else:
+        return f'Ну и сиди скучай!'
 
 def game_city(client_message_city):
     """
@@ -34,6 +54,5 @@ def game_city(client_message_city):
 
     return city_result
 
-
-print(game_city(city))
+print(first_start_game('$Игра'))
 print(cities)
