15bf1604727e8287d6e49dcef8a0b9bde9b805c0
MotRM <motr.89@mail.ru>
2021-04-20 14:46:21 +1000

    Исправлена ошибка связанная с Try. Интерфейс переделан с Client на Bot.command В интерфейс добавлена функция игры, все работает!

--- corebot.py
+++ corebot.py
@@ -17,10 +17,23 @@ async def Привет(ctx):
 async def Игра(ctx):
     await ctx.send(gamecity.first_start_game())
 
+@bot.command(pass_context=True)
+async def Чистка(ctx, amount=100):
+    await ctx.channel.purge(limit=amount)
 
-@bot.command()
+@bot.event
 async def on_message(message):
+    msg = message.content
+
     if message.author == bot.user:
         return
 
+    if msg == 'Игра':
+        @bot.event
+        async def on_message(message):
+            if message.author == bot.user:
+                return
+            await message.channel.send(gamecity.game_city(message.content))
+
+    await bot.process_commands(message)
 bot.run('')
