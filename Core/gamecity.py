f905c28eb75e3434f979f1aa397b00131730c50c
MotRM <motr.89@mail.ru>
2021-04-28 15:55:55 +1000

    Восстановлен репозиторий после потери пути. Добавлено три кога, для команд, игры и информации. Удалены старые модули

--- Core/gamecity.py
+++ Core/gamecity.py
@@ -7,7 +7,7 @@ city_key_client = ''
 first_city = ''
 first_city_key = ''
 
-with open('CITIES.md', 'r', encoding='Windows-1251') as file:
+with open('C:/Users/motr8/Desktop/Projects/Core/CITIES.md', 'r', encoding='Windows-1251') as file:
     for line in file:
         cities[line[0].lower()].append(line[:-1])
 
@@ -18,13 +18,14 @@ def first_start_game():
      город первым. """
 
     if random.randint(0, 5) > 2:
-        return f'Вы называете город первым!'
+        return f'Вы называете город первым!\n' \
+               f'Чтобы закончить наберите +Закончить'
     else:
         random_values_city = random.choice(list(cities.values()))
         first_city = random_values_city[random.randint(0, len(random_values_city) - 1)]
         first_city_key = first_city[0].lower()
         cities[first_city_key].remove(first_city)
-        return f'Я хожу первым!\nГород: {first_city}\nЧтобы закончить наберите Закончить'
+        return f'Я хожу первым!\nГород: {first_city}\nЧтобы закончить наберите +Закончить'
 
 def game_city(client_message_city):
     """
@@ -53,7 +54,7 @@ def game_city(client_message_city):
 
 def clear_dict_cities():
     cities.clear()
-    with open('CITIES.md', 'r', encoding='Windows-1251') as file:
+    with open('C:/Users/motr8/Desktop/Projects/Core/CITIES.md', 'r', encoding='Windows-1251') as file:
         for line in file:
             cities[line[0].lower()].append(line[:-1])
 
